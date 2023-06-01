3a7b8aaa15025d2ae28123056068e530da02d333
fengjiachun <jiachun_feng@proton.me>
2023-06-01 02:00:55 +0000

    Deploying to gh-pages from @ GreptimeTeam/greptimedb@70e17ead681431494ce9b74ccd260ec006bba249 🚀

--- table_procedure/error/sidebar-items.js
+++ table_procedure/error/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Error"],"struct":["AccessCatalogSnafu","CatalogNotFoundSnafu","DeserializeProcedureSnafu","InvalidRawSchemaSnafu","SchemaNotFoundSnafu","SerializeProcedureSnafu","SubprocedureFailedSnafu","TableExistsSnafu","TableNotFoundSnafu"],"type":["Result"]};
+window.SIDEBAR_ITEMS = {"enum":["Error"],"struct":["AccessCatalogSnafu","CatalogNotFoundSnafu","DeserializeProcedureSnafu","InvalidRawSchemaSnafu","SchemaNotFoundSnafu","SerializeProcedureSnafu","TableExistsSnafu","TableNotFoundSnafu"],"type":["Result"]};
