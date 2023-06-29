91922af3d444750b7f79bad265fe139bc3527ec0
Fengys123 <40801205+Fengys123@users.noreply.github.com>
2023-06-29 08:57:48 +0000

    Deploying to gh-pages from @ GreptimeTeam/greptimedb@2b3ca1309a842a91f6fc92cfbf3d8b8b6964b7d1 🚀

--- meta_srv/table_routes/sidebar-items.js
+++ meta_srv/table_routes/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["batch_get_table_global_value","get_table_global_value","get_table_route_value","put_table_global_value","put_table_route_value"]};
+window.SIDEBAR_ITEMS = {"fn":["batch_get_table_global_value","get_table_global_value","get_table_route_value","move_value","put_table_global_value","put_table_route_value","remove_table_global_value","remove_table_route_value"]};
