0f9b924c7fe804716575366728a84a80e751a6c9
v0y4g3r <6406592+v0y4g3r@users.noreply.github.com>
2023-07-06 08:28:15 +0000

    Deploying to gh-pages from @ GreptimeTeam/greptimedb@dc085442d70fedd97bd73e111554ee02ce055144 🚀

--- implementors/common_meta/kv_backend/trait.KvBackend.js
+++ implementors/common_meta/kv_backend/trait.KvBackend.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"catalog":[["impl KvBackend for <a class=\"struct\" href=\"catalog/remote/client/struct.MetaKvBackend.html\" title=\"struct catalog::remote::client::MetaKvBackend\">MetaKvBackend</a>"],["impl KvBackend for <a class=\"struct\" href=\"catalog/remote/client/struct.CachedMetaKvBackend.html\" title=\"struct catalog::remote::client::CachedMetaKvBackend\">CachedMetaKvBackend</a>"]],
+"catalog":[["impl KvBackend for <a class=\"struct\" href=\"catalog/remote/client/struct.CachedMetaKvBackend.html\" title=\"struct catalog::remote::client::CachedMetaKvBackend\">CachedMetaKvBackend</a>"],["impl KvBackend for <a class=\"struct\" href=\"catalog/remote/client/struct.MetaKvBackend.html\" title=\"struct catalog::remote::client::MetaKvBackend\">MetaKvBackend</a>"]],
 "common_meta":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
